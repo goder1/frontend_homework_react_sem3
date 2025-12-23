19060bbb3266a2f2738fa597c5b0d9ee391bfc2b
Babin Daniil <goder_228@mail.com>
2025-12-23 19:14:08 +0300

    temp

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,9 +7,6 @@ import styles from './Layout.module.css';
 const Layout: React.FC = () => {
   return (
     <div className={styles.layout}>
-      <a href="/presentation" className="presentation-link">
-        📊 Презентация
-      </a>
       <Header />
       <main className={styles.mainContent}>
         <Outlet /> {/* Заменяем children на Outlet */}
